a4b52f456e0c8521e673a1426c400b18ed1fecd1
magnusjoenssonfragt <magnus.joensson@fragt.dk>
2026-08-10 14:09:38 +0200

    Update streamlit_app.py
    Added search-on column and error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,9 @@ st.write(
 )
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie order wille be: ', name_on_order)
